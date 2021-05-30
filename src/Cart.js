7a1f4cd7c286c8f95e7b7a0d3d61ee19c6f73dbc
Keshav Jaiswal <keshav.jaiswal39@gmail.com>
2021-05-30 20:13:11 +0530

    Increase and decrease quantity of cart item

--- src/Cart.js
+++ src/Cart.js
@@ -34,17 +34,47 @@ class Cart extends React.Component {
                 ]
             }
         }
+
+        handleIncreaseQuantity = (product) => {
+            console.log('Hey , please inc the quantity of the' , product);
+            const {products} = this.state;
+            const index = products.indexOf(product);
+
+            products[index].qty += 1;
+
+            this.setState ({
+                products: products
+            })
+        }
+
+        handleDecreaseQuantity = (product) => {
+            console.log('Hey , please inc the quantity of the' , product);
+            const {products} = this.state;
+            const index = products.indexOf(product);
+
+            if (products[index].qty === 0) {
+                return;
+            }
+
+            products[index].qty -= 1;
+
+            this.setState ({
+                products: products
+            })
+        }
+    
     render () {
         const { products } = this.state;
         return (
             <div className="cart">
                 {
                     products.map((product) => {
-                        return 
-                        (
+                        return (
                             <CartItem 
                                 product={product} 
                                 key={product.id}
+                                onIncreaseQuantity={this.handleIncreaseQuantity}
+                                onDecreaseQuantity={this.handleDecreaseQuantity}
                             />
                         )
 
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,115 +1,116 @@
 import React from 'react';
 
 class CartItem extends React.Component {
-    // 2. we added this state
+    // // 2. we added this state
 
-    // we dont need this state over here, instead we will use it in cart.js
-    // constructor() {
-    //     super(); // this will call the constructor of my component class in react, 
-    //     //  so whenever we are using constructor in our classes, we need to call super() to basically call the constructor of my parent class if I am inheriting
-    //     this.state = {
-    //         price: 9999,
-    //         title: 'Mobile Phone',
-    //         qty: 0,
-    //         img: ''
-    //     }
-    //     // Second method is to do binding here
-    //     // this.increaseQuantity = this.increaseQuantity.bind(this);
+    // // we dont need this state over here, instead we will use it in cart.js
+    // // constructor() {
+    // //     super(); // this will call the constructor of my component class in react, 
+    // //     //  so whenever we are using constructor in our classes, we need to call super() to basically call the constructor of my parent class if I am inheriting
+    // //     this.state = {
+    // //         price: 9999,
+    // //         title: 'Mobile Phone',
+    // //         qty: 0,
+    // //         img: ''
+    // //     }
+    // //     // Second method is to do binding here
+    // //     // this.increaseQuantity = this.increaseQuantity.bind(this);
 
-    //     // lets call the testing function here to check how its synchronous
-    //     // this.testing();
-    // }
+    // //     // lets call the testing function here to check how its synchronous
+    // //     // this.testing();
+    // // }
 
-    // ************************************************************************************************************
+    // // ************************************************************************************************************
 
-    // example to understand that react does not perform batching all the time, like in some cases like, ajax call, or while using promises
+    // // example to understand that react does not perform batching all the time, like in some cases like, ajax call, or while using promises
 
-    // testing () {
-    //     const promise = new Promise ((resolve, reject) => {
-    //         setTimeout(() => {
-    //             resolve('done');
-    //         }, 5000)
-    //     })
+    // // testing () {
+    // //     const promise = new Promise ((resolve, reject) => {
+    // //         setTimeout(() => {
+    // //             resolve('done');
+    // //         }, 5000)
+    // //     })
 
-    //     promise.then(() => {
-    //         // setState acts like a synchronous call
-    //         // this.setState({qty: 100});
+    // //     promise.then(() => {
+    // //         // setState acts like a synchronous call
+    // //         // this.setState({qty: 100});
 
-    //         // lets make some changes and call it thrice to see what happens next
-    //         // this.setState({qty: this.state.qty + 10});
-    //         // this.setState({qty: this.state.qty + 10});
-    //         // this.setState({qty: this.state.qty + 10});
+    // //         // lets make some changes and call it thrice to see what happens next
+    // //         // this.setState({qty: this.state.qty + 10});
+    // //         // this.setState({qty: this.state.qty + 10});
+    // //         // this.setState({qty: this.state.qty + 10});
 
-    //         // this time, our component will get re-rendered thrice and there is not batching
+    // //         // this time, our component will get re-rendered thrice and there is not batching
 
-    //         console.log('state',this.state);
+    // //         console.log('state',this.state);
+    // //     });
+    // // }
+
+
+
+
+
+    // // ************************************************************************************************************
+
+    // // Increase Quantity
+
+    // // easy method rather than doing binding is placing an arrow function
+
+    // increaseQuantity = () => {
+    //     // here it will only increase the value in the state, but it wont re-render in our component so we will use setState funciton to re-render in our component
+    //     // this.state.qty += 1;
+
+    //     // setState method - 1
+    //     // this.setState({
+    //     //     qty: this.state.qty + 1   // it does shallow merging, it means, if i want to change the quantity then it will only change the qunantity, it wont touch other properties
+    //     // });
+
+    //     // setState method - 2
+    //     // Instead of passing an object, we can pass a function
+
+    //     // So, if i require a previous state i will use fucntion form, else i will use the object form
+    //     this.setState((prevState) => {
+    //         return {
+    //             qty: prevState.qty + 1
+    //         }
+    //     // As our setState method is asynchronous, we cannot rely on the above line, instead we wil pass one more argument here
+    //     // }, () => {
+    //     //     console.log('this.state', this.state);
     //     });
+
+    //     // console.log('this.state', this.state);
     // }
 
+    // // Decrease Quantity
 
+    // decreaseQuantity = () => {
+    //     // one point to check over here is, if our current quantity is 0, then we dnt want to call this function
 
-
-
-    // ************************************************************************************************************
-
-    // Increase Quantity
-
-    // easy method rather than doing binding is placing an arrow function
-
-    increaseQuantity = () => {
-        // here it will only increase the value in the state, but it wont re-render in our component so we will use setState funciton to re-render in our component
-        // this.state.qty += 1;
-
-        // setState method - 1
-        // this.setState({
-        //     qty: this.state.qty + 1   // it does shallow merging, it means, if i want to change the quantity then it will only change the qunantity, it wont touch other properties
-        // });
-
-        // setState method - 2
-        // Instead of passing an object, we can pass a function
-
-        // So, if i require a previous state i will use fucntion form, else i will use the object form
-        this.setState((prevState) => {
-            return {
-                qty: prevState.qty + 1
-            }
-        // As our setState method is asynchronous, we cannot rely on the above line, instead we wil pass one more argument here
-        // }, () => {
-        //     console.log('this.state', this.state);
-        });
-
-        // console.log('this.state', this.state);
-    }
-
-    // Decrease Quantity
-
-    decreaseQuantity = () => {
-        // one point to check over here is, if our current quantity is 0, then we dnt want to call this function
-
-        // destructuring the qty
-        const { qty } = this.state;
+    //     // destructuring the qty
+    //     const { qty } = this.state;
         
-        if(qty === 0 )
-        {
-            return;
-        }
+    //     if(qty === 0 )
+    //     {
+    //         return;
+    //     }
 
-        this.setState((prevState) => {
-            return {
-                qty: prevState.qty - 1
-            }
-            // As our setState method is asynchronous, we cannot rely on the above line, instead we wil pass one more argument here
-            // }, () => {
-            //     console.log('this.state', this.state);
-            });
+    //     this.setState((prevState) => {
+    //         return {
+    //             qty: prevState.qty - 1
+    //         }
+    //         // As our setState method is asynchronous, we cannot rely on the above line, instead we wil pass one more argument here
+    //         // }, () => {
+    //         //     console.log('this.state', this.state);
+    //         });
     
-            // console.log('this.state', this.state);
-        }
+    //         // console.log('this.state', this.state);
+    //     } 
 
     render() {
         console.log('this.props',this.props)
         //  Instead of writing this.state.title everywhere, we can define all those and get those from my object and i will use object destructuring
         const { price, title, qty } = this.props.product;
+        const { product, onIncreaseQuantity, onDecreaseQuantity } = this.props;
         return (
 
             // 1. we added this JsX first
@@ -130,13 +131,13 @@ class CartItem extends React.Component {
                             // 3. we saw how to attach click listener and discussed about binding
                             // onClick={this.increaseQuantity}    // this will show error so we will do binding
                             // onClick={this.increaseQuantity.bind(this)}    // 1st way is to to binding here.
-                            onClick={this.increaseQuantity}
+                            onClick={() => onIncreaseQuantity(product)}
                         />
                         <img 
                             alt="decrease" 
                             className="action-icons" 
                             src="https://image.flaticon.com/icons/png/128/992/992683.png" 
-                            onClick={this.decreaseQuantity}
+                            onClick={() => onDecreaseQuantity(product)}
                         />
                         <img 
                             alt="delete" 
