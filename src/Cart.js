c059b0f3b69e94181cb9e0dc05fc50da5d7e701a
Keshav Jaiswal <keshav.jaiswal39@gmail.com>
2021-05-30 14:44:06 +0530

    render a list in Card.js

--- src/Cart.js
+++ src/Cart.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import CartItem from './CartItem';
 
+// class Cart extends React.Component {
+//     render () {
+//         return (
+//             <div className="cart">
+//                 <CartItem/>
+//                 <CartItem/>
+//                 <CartItem/>
+//             </div>
+//         );
+//     }
+// }
+
 class Cart extends React.Component {
     render () {
+        const arr = [1,2,3,4,5];
         return (
             <div className="cart">
-                <CartItem/>
-                <CartItem/>
-                <CartItem/>
+            {/* // this will return the array item */}
+                {/* {arr} */}
+
+                {/* lets try to increase all the item in the array by 5 */}
+                { 
+                    arr.map((item) => {
+                        return item + 5;
+                    })
+                }
             </div>
         );
     }
